a41cc008f3b58d5785c2f763e5fb6a988e0cf24f
joelonglin <joelonglin@foxmail.com>
2021-06-09 00:40:22 +0800

    initial clean

--- gluonts/lzl_data_from_gluonts/data/__init__.py
+++
@@ -1,8 +0,0 @@
-from .data_utils import whetherInputInList
-
-__all__ = [
-    'whetherInputInList'
-]
-for item in __all__:
-    if hasattr(item, "__module__"):
-        setattr(item, "__module__", __name__)
